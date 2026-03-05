a765e6681b22209b3316f994729d000d09dc2115
DzopSon <oliwier523@interia.pl>
2026-03-05 11:22:33 +0100

    1

--- Calculation/Class1.cs
+++ Calculation/Class1.cs
@@ -1,7 +1,31 @@
-﻿namespace Calculation
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace Calculation
+{
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Nie mozna dzielic przez zero");
+            }
+            return a / b;
+        }
     }
 }
